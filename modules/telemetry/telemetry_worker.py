3bcbd0c0ca9a195ddb5466d87e9844bc20e66ba2
Tianze Yin <84604437+tianzeyin@users.noreply.github.com>
2025-10-29 20:47:25 -0400

    Add controller and telemetry_queue to telemetry_worker
    Updated telemetry_worker function to include controller and telemetry_queue parameters with descriptions.

--- modules/telemetry/telemetry_worker.py
+++ modules/telemetry/telemetry_worker.py
@@ -18,13 +18,17 @@ from ..common.modules.logger import logger
 # =================================================================================================
 def telemetry_worker(
     connection: mavutil.mavfile,
-    args,  # Place your own arguments here
+    controller: worker_controller.WorkerController,
+    telemetry_queue: queue_proxy_wrapper.QueueProxyWrapper,  # Place your own arguments here
     # Add other necessary worker arguments here
 ) -> None:
     """
     Worker process.
 
     args... describe what the arguments are
+    connection indicates the channel between the drone
+    controller regulates the worker state
+    telemetry_queue stores telemetry data for access
     """
     # =============================================================================================
     #                          ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -47,8 +51,19 @@ def telemetry_worker(
     #                          ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
     # =============================================================================================
     # Instantiate class object (telemetry.Telemetry)
+    result, telemetry_instance = telemetry.Telemetry.create(connection, local_logger)
+    if not result:
+        local_logger.error("Failed to create telemetry object")
+        return
 
     # Main loop: do work.
+    while (
+        not controller.is_exit_requested()
+    ):  # i love documentation file for letting me spam this ult
+        controller.check_pause()
+        value = telemetry_instance.run()
+        if value:
+            telemetry_queue.queue.put(value)
 
 
 # =================================================================================================
