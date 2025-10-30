e80bdf7ed181f306252398e0f5176bd8decc4acc
Tianze Yin <84604437+tianzeyin@users.noreply.github.com>
2025-10-29 20:07:28 -0400

    Add create method for HeartbeatSender with error handling
    Implemented the create method to instantiate HeartbeatSender and handle exceptions during creation.

--- modules/heartbeat/heartbeat_sender.py
+++ modules/heartbeat/heartbeat_sender.py
@@ -18,32 +18,39 @@ class HeartbeatSender:
     @classmethod
     def create(
         cls,
-        connection: mavutil.mavfile,
-        args,  # Put your own arguments here
+        connection: mavutil.mavfile,  # Put your own arguments here
     ) -> "tuple[True, HeartbeatSender] | tuple[False, None]":
         """
         Falliable create (instantiation) method to create a HeartbeatSender object.
         """
-        pass  # Create a HeartbeatSender object
+        try:
+            heartbeat_sender = cls(cls.__private_key, connection)
+            return True, heartbeat_sender
+        except (OSError, mavutil.mavlink.MAVError) as exception:
+            print(
+                f"Heartbeat receiver object creation failed: {exception}"
+            )  # Using print since logger was not imported
+            return False, None  # Create a Heartbeat Sender object
 
     def __init__(
         self,
         key: object,
-        connection: mavutil.mavfile,
-        args,  # Put your own arguments here
-    ):
+        connection: mavutil.mavfile,  # Put your own arguments here
+    ) -> None:
         assert key is HeartbeatSender.__private_key, "Use create() method"
 
         # Do any intializiation here
+        self.connection = connection
 
     def run(
-        self,
-        args,  # Put your own arguments here
-    ):
+        self,  # Put your own arguments here
+    ) -> None:
         """
         Attempt to send a heartbeat message.
         """
-        pass  # Send a heartbeat message
+        self.connection.mav.heartbeat_send(
+            mavutil.mavlink.MAV_TYPE_GCS, mavutil.mavlink.MAV_AUTOPILOT_INVALID, 0, 0, 0
+        )  # Send a heartbeat message
 
 
 # =================================================================================================
