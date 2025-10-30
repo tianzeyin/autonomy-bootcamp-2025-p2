480922501de522d14c5721d5a364fdaa4fa28fd9
Tianze Yin <84604437+tianzeyin@users.noreply.github.com>
2025-10-30 17:13:12 -0400

    Refactor stop function and integrate WorkerController
    Refactor stop function to use WorkerController and remove unnecessary arguments.

--- tests/integration/test_heartbeat_sender.py
+++ tests/integration/test_heartbeat_sender.py
@@ -26,6 +26,7 @@ NUM_TRIALS = 10
 #                            ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
 # =================================================================================================
 # Add your own constants here
+controller = worker_controller.WorkerController()
 
 # =================================================================================================
 #                            ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
@@ -44,13 +45,11 @@ def start_drone() -> None:
 # =================================================================================================
 #                            ↓ BOOTCAMPERS MODIFY BELOW THIS COMMENT ↓
 # =================================================================================================
-def stop(
-    args,  # Add any necessary arguments
-) -> None:
+def stop() -> None:
     """
     Stop the workers.
     """
-    pass  # Add logic to stop your worker
+    controller.request_exit()  # Add logic to stop your worker
 
 
 # =================================================================================================
@@ -95,10 +94,12 @@ def main() -> int:
     # Create a worker controller for your worker
 
     # Just set a timer to stop the worker after a while, since the worker infinite loops
-    threading.Timer(HEARTBEAT_PERIOD * NUM_TRIALS, stop, (args,)).start()
+    threading.Timer(HEARTBEAT_PERIOD * NUM_TRIALS, stop).start()
 
     heartbeat_sender_worker.heartbeat_sender_worker(
         # Place your own arguments here
+        connection,
+        controller,
     )
     # =============================================================================================
     #                          ↑ BOOTCAMPERS MODIFY ABOVE THIS COMMENT ↑
